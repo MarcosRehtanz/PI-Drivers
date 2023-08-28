f7349172dbe5a3bb826f4fb9fd1c2e7e6ca9d143
Mansilla <marcos.rehtanz@gmail.com>
2023-08-28 20:42:30 -0300

    feat(Form): Submit the completed form to server

--- client/src/pages/Form/Form.jsx
+++ client/src/pages/Form/Form.jsx
@@ -1,55 +1,107 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Form.css'
 
 
 export const Form = () => {
 
+    const [driver, setDriver] = useState({
+        name: '',
+        surname: '',
+        nationality: '',
+        image: '',
+        birthdate: '',
+        description: '',
+        teams: '',
+    })
+
+    const handleChange = ({ target }) => {
+        setDriver(oldDriver => {
+            return {
+                ...oldDriver,
+                [target.name]: target.value
+            }
+        })
+    }
+
+    const [error, setError] = useState({})
+    const validate = () => {
+        let err = {}
+        if( !driver.name) err.name = 'Campo Obligatorio'
+        if( !driver.surname) err.surname = 'Campo Obligatorio'
+        if( !driver.nationality) err.nationality = 'Campo Obligatorio'
+        // //if( !driver.image) err.image = 'Campo Obligatorio'
+        if( !driver.birthdate) err.birthdate = 'Campo Obligatorio'
+        if( !driver.description) err.description = 'Campo Obligatorio'
+        if( !driver.teams) err.teams = 'Campo Obligatorio'
+
+        setError(()=>err)
+    }
+
+    const navigate = useNavigate()
+    const submit = async() => {
+        try {
+            await axios.post('http://localhost:3001/drivers', driver)
+            alert('Suscripción exitosa')
+            navigate('/home')
+        } catch (error) {
+            alert(error.message)
+            navigate('/home')
+        }
+    }
+
+    useEffect(()=> {
+        validate()
+    }, [driver])
+
     return (
         <div>
-            <form >
+            <form onSubmit={e => e.preventDefault()} >
 
                 <div className='input-section' >
                     <label className='label-name' >Nombre</label>
-                    <input className='input' type="text" />
+                    <input value={driver.name} onChange={handleChange} name="name" className='input' type="text" />
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.name ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section' >
                     <label className='label-name' >Apellido</label>
-                    <input className='input' type="text" />
+                    <input value={driver.surname} onChange={handleChange} name="surname" className='input' type="text" />
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.surname ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section' >
                     <label className='label-name' >Nacionalidad</label>
-                    <input className='input' type="text" />
+                    <input value={driver.nationality} onChange={handleChange} name="nationality" className='input' type="text" />
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.nationality ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section' >
                     <label className='label-name' >Imagen</label>
-                    <input className='input' type="text" />
+                    <input value={driver.image} onChange={handleChange} name="image" className='input' type="text" />
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.image ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section'>
                     <label className='label-name' >Fecha de Nacimiento</label>
-                    <input className='input input-date' type="date" />
+                    <input value={driver.birthdate} onChange={handleChange} name="birthdate" className='input input-date' type="date" />
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.birthdate ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section input-section-date' >
                     <label className='label-name' >Descripción:</label>
-                    <textarea className='input' name="" id="" cols="30" rows="10"></textarea>
+                    <textarea value={driver.description} onChange={handleChange} name="description" className='input' cols="30" rows="10"/>
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.description ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section' >
                     <label className='label-name' >Escuderías</label>
-                    <input className='input' type="text" />
+                    <input value={driver.teams} onChange={handleChange} name="teams" className='input' type="text" />
                 </div>
-                {true ? <br /> : <label className='error-message'>error</label>}
+                {!error.teams ? <br /> : <label className='error-message'>error</label>}
 
-                <button>Submit</button>
+                <button onClick={submit} >Submit</button>
             </form>
         </div>
     )
