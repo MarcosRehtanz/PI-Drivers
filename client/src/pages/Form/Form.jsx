3d305f9e813281a47ccbb26bdbf206a8340be7e0
Mansilla <marcos.rehtanz@gmail.com>
2023-09-01 17:24:26 -0300

    Fix(Form): Fix the input by teams / Now you can to select multiple teams in the form

--- client/src/pages/Form/Form.jsx
+++ client/src/pages/Form/Form.jsx
@@ -20,34 +20,65 @@ export const Form = () => {
     const [error, setError] = useState({})
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const allTeams = useSelector(state=>state.allTeams)
+    const allTeams = useSelector(state => state.allTeams)
 
     const handleChange = ({ target }) => {
-        setDriver(oldDriver => {
-            return {
-                ...oldDriver,
-                [target.name]: target.value
-            }
-        })
+
+        if (target.name === 'teams') {
+            target.value == 0
+                ? console.log('Estoy Vacio')
+                : setDriver(oldDriver => {
+                    console.log(driver.teams === ''
+                        ? target.value
+                        : !driver.teams.includes(target.value)
+                            ? driver.teams + ', ' + target.value
+                            : driver.teams);
+
+                    return {
+                        ...oldDriver,
+                        teams: !driver.teams
+                            ? target.value
+                            : !driver.teams.includes(target.value)
+                                ? driver.teams + ', ' + target.value
+                                : driver.teams
+                    }
+                })
+        } else {
+
+            setDriver(oldDriver => {
+                return {
+                    ...oldDriver,
+                    [target.name]: target.value
+                }
+            })
+        }
+
+    }
+    const removeTeam = ({target}) => {
+        setDriver( oldDriver => ({
+            ...oldDriver,
+            teams: oldDriver.teams.split(', ').filter(team => team !== target.name).join(', ')
+        }))
+        console.log(target.name);
     }
 
     const validate = () => {
         let err = {}
-        if( !driver.name) err.name = 'Campo Obligatorio'
-        if( !driver.surname) err.surname = 'Campo Obligatorio'
-        if( !driver.nationality) err.nationality = 'Campo Obligatorio'
+        if (!driver.name) err.name = 'Campo Obligatorio'
+        if (!driver.surname) err.surname = 'Campo Obligatorio'
+        if (!driver.nationality) err.nationality = 'Campo Obligatorio'
         // //if( !driver.image) err.image = 'Campo Obligatorio'
-        if( !driver.birthdate) err.birthdate = 'Campo Obligatorio'
-        if( !driver.description) err.description = 'Campo Obligatorio'
-        if( !driver.teams) err.teams = 'Campo Obligatorio'
+        if (!driver.birthdate) err.birthdate = 'Campo Obligatorio'
+        if (!driver.description) err.description = 'Campo Obligatorio'
+        if (!driver.teams) err.teams = 'Campo Obligatorio'
 
-        setError(()=>err)
-        console.log(Object.keys(error).length);
+        setError(() => err)
+        //console.log(Object.keys(error).length);
     }
 
     const submit = () => {
         try {
-            if(Object.keys(error).length>0){
+            if (Object.keys(error).length > 0) {
                 alert('Completa todos los campos para continuar el registro')
                 return;
             }
@@ -61,7 +92,7 @@ export const Form = () => {
         }
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         validate()
     }, [driver])
 
@@ -89,7 +120,7 @@ export const Form = () => {
 
                 <div className='input-section' >
                     <label className='label-name' >Imagen</label>
-                    <input value={driver.image} onChange={handleChange} name="image" className='input' type="file" />
+                    <input value={driver.image} onChange={handleChange} name="image" className='input' type="url" />
                 </div>
                 {!error.image ? <br /> : <label className='error-message'>error</label>}
 
@@ -101,17 +132,24 @@ export const Form = () => {
 
                 <div className='input-section input-section-date' >
                     <label className='label-name' >Descripción:</label>
-                    <textarea value={driver.description} onChange={handleChange} name="description" className='input' cols="30" rows="10"/>
+                    <textarea value={driver.description} onChange={handleChange} name="description" className='input' cols="30" rows="10" />
                 </div>
                 {!error.description ? <br /> : <label className='error-message'>error</label>}
 
                 <div className='input-section' >
                     <label className='label-name' >Escuderías</label>
                     <select onChange={handleChange} name="teams" id="">
-                        {allTeams.map( team => <option value={team.name} >{team.name}</option>)}
+                        <option value='0'>Select a Team *</option>
+                        {allTeams.map(team => <option value={team.name} >{team.name}</option>)}
                     </select>
                 </div>
-                {!error.teams ? <br /> : <label className='error-message'>error</label>}
+                {error.teams 
+                    ? <label className='error-message'>error</label>
+                    : driver.teams.split(', ').map((team) => <>
+                        -<button onClick={removeTeam} className='team' name={team}>{team}</button>- 
+                    </>)
+                }
+                <br />
 
                 <button onClick={submit} >Submit</button>
             </form>
