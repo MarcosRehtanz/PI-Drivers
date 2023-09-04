4d2121aed611e28e4959de91dda98ed33ebde8f4
Mansilla <marcos.rehtanz@gmail.com>
2023-09-04 18:29:28 -0300

    Style(DriverCard): Fix the Cards's style

--- client/src/components/DriverCard/DriverCard.jsx
+++ client/src/components/DriverCard/DriverCard.jsx
@@ -6,8 +6,10 @@ export const DriverCard = ({ id, name, surname, image, teams }) => {
     return (
 
         <Link to={`/drivers/${id}`} id={id} className='DriverCard-container navigate-item' >
-            <h1 className='DriverCard-name'>{name} {surname}</h1>
-            <div className='DriverCard-image'><img className='Driver-image' src={image} alt="foto" /></div>
+            <div className='DriverCard-name'><h1 className='Driver-name'>{name} {surname}</h1></div>
+            <div className='DriverCard-image'>
+                <img className='Driver-image' src={image} alt="foto" />
+            </div>
             {teams ? <p className='DriverCard-data'>{teams.map((team) => team.name).join(', ')}</p> : ''}
             {/* {teams ? <div className='DiverCard-data'>{teams.map((team, i) => <p key={i}>{team.name}</p>)}</div> : ''} */}
         </Link>
