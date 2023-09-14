f463c209951a962e46e74a574a6bf38d4e591ff0
Mansilla <marcos.rehtanz@gmail.com>
2023-09-13 22:02:04 -0300

    Fix(server): Fix duplicate driver on create

--- server/src/controllers/postDriver.js
+++ server/src/controllers/postDriver.js
@@ -1,24 +1,45 @@
 const { Driver, Team } = require('../db')
+const { Op } = require('sequelize')
 
 module.exports = async (req, res) => {
 
-    const newDriver = req.body
-    if (!newDriver.name || !newDriver.surname || !newDriver.description || !newDriver.nationality || !newDriver.birthdate || !newDriver.teams){
+    const { name, surname, birthdate, description, image, nationality, teams } = req.body
+    if (!name || !surname || !description || !nationality || !birthdate || !teams) {
         return res.status(400).send('Incomplete information')
     }
 
-    
+
     try {
-        const driver = await Driver.create(newDriver)
-        
-        newDriver.teams.map( async name => {
-            const team = await Team.findOne( { where: {name: name} } )
+        const [driver, created] = await Driver.findOrCreate({
+            where: {
+                name: {
+                    [Op.iLike]: name
+                },
+
+                surname: {
+                    [Op.iLike]: surname
+                },
+                birthdate
+            },
+            defaults: {
+                name,
+                surname,
+                description,
+                nationality,
+                image,
+            }
+        })
+        console.log('Mensage', driver);
+        if (!created) return res.status(409).send('User exist')
+
+        teams.map(async name => {
+            const team = await Team.findOne({ where: { name: name } })
             driver.addTeam(team)
-        } )
+        })
 
         res.status(200).send(driver)
     } catch (error) {
-        res.status(500).send({error: error.message})
+        res.status(500).send({ error: error.message })
     }
 
 }
